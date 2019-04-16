8b3b633e6691bd64e98aa9037072e06f2e287186
Amy Cen <amy.cen@blackrock.com>
2019-04-16 21:46:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,8 @@ def get_first_name_of_season_winner(data, season)
   # code here
   winner_name = ""
   data[season].each do |contestants|
-    winner_name = contestants["name"].split if ["status"] == "Winner"[0]
+    winner_name = contestants["name"].split if ["status"] == "Winner"
+    winner_name = winner_name[0]
   end
   winner_name
 end
