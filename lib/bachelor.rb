547df69946b9d288912c07484d11e7e22e015f72
Amy Cen <amy.cen@blackrock.com>
2019-04-16 21:45:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   # code here
   data[season].map do |contestants|
-    contestants["name"].split[0] if ["status"] == "Winner".compact
+    (contestants["name"].split[0] if ["status"] == "Winner").compact
   end
 end
 
