3a8c60987648ae68cf373cdd842d2742cda201d5
Amy Cen <amy.cen@blackrock.com>
2019-04-16 21:50:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,6 +18,11 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |contestants|
+    count += 1 if contestants["hometown"] == hometown
+  end
+  count
 end
 
 def get_occupation(data, hometown)
