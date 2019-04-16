d53766c771d32669a02f70867b046367e29616bb
Amy Cen <amy.cen@blackrock.com>
2019-04-16 22:05:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,7 @@ def get_occupation(data, hometown)
       occupation = contestant["occupation"] if contestant["hometown"] == hometown
     end
   end
-  name
+  occupation
 end
 
 def get_average_age_for_season(data, season)
